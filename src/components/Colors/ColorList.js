daed5672c3c73657bcb4b9a67603cb1911fee84f
Toufiquer <toufiquerabc@gmail.com>
2023-01-12 02:04:56 +0600

    color plate design only component complere

--- src/components/Colors/ColorList.js
+++ src/components/Colors/ColorList.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { copyTextToClipboard } from "../utilities/copyTextToClipboard";
 import ColorBox from "./ColorBox";
 
-const ColorList = ({ styleColor, data, children }) => {
+const ColorList = ({ styleColor, data }) => {
+  const [tooltipTime, setTooltipTime] = useState(false);
   const colorCode = data.split("-")[1];
-
+  const showTooltip = () => {
+    setTooltipTime(!tooltipTime);
+    setTimeout(() => {
+      setTooltipTime(tooltipTime);
+    }, 1300);
+  };
+  const styleTooltip = "w-[240px] h-6 bg-red-600 absolute top-[-25px] flex item s-center justify-center ";
+  const tooltip = <div className={`${tooltipTime ? " block " : " hidden "} + ${styleTooltip}`}>Code has been copied {colorCode}</div>;
   return (
-    <li onClick={() => copyTextToClipboard(colorCode)} title="click to copy #[hex]" className={styleColor}>
+    <li
+      onClick={() => {
+        copyTextToClipboard(colorCode);
+        showTooltip();
+      }}
+      title="click to copy #[hex]"
+      className={styleColor + " bg-blue-200 hover:bg-blue-500"}
+    >
       {data.split("-")[0]} <ColorBox data={colorCode}></ColorBox>
-      {children}
+      {tooltip}
     </li>
   );
 };
--- src/components/Colors/SingleColorList.js
+++ src/components/Colors/SingleColorList.js
@@ -1,16 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import ColorList from "./ColorList";
 
 const SingleColorList = ({ data }) => {
-  const [tooltipTime, setTooltipTime] = useState(false);
   const styleColor = "relative flex h-8 w-[280px] my-4 pl-2 cursor-pointer justify-between border-2 border-solid border-blue-800 items-center justify-center";
-  const styleTooltip = "w-[240px] h-6 bg-red-600 absolute top-[-25px] flex item s-center justify-center ";
   const dataArr = [];
   for (const curr in data.color) {
     const newData = curr.concat("-").concat(data.color[curr]);
     dataArr.push(newData);
   }
-  const tooltip = <div className={`${tooltipTime ? " block " : " hidden "} + ${styleTooltip}`}>Code has been copied</div>;
   return (
     <>
       <ul>
@@ -19,9 +16,7 @@ const SingleColorList = ({ data }) => {
         </li>
 
         {dataArr.map((curr) => (
-          <ColorList key={curr} styleColor={styleColor} data={curr}>
-            {tooltip}
-          </ColorList>
+          <ColorList key={curr} styleColor={styleColor} data={curr}></ColorList>
         ))}
       </ul>
     </>
