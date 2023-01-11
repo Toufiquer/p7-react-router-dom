4f0c6382631c21fd9f82e1b7056f3f194ae044cd
Toufiquer <toufiquerabc@gmail.com>
2023-01-12 02:46:02 +0600

    navbar lite and dark complete

--- src/components/CustomLink/CustomLink.js
+++ src/components/CustomLink/CustomLink.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import { ThemeMode } from "../../App";
 
 const CustomLink = ({ children, to, ...props }) => {
+  const [theme] = useContext(ThemeMode);
+  console.log(theme, " => Line No: 7");
   let resolved = useResolvedPath(to);
   let match = useMatch({ path: resolved.pathname, end: true });
-  const style = " px-4 text-xl hover:text-white duration-200 ";
-  const active = style.concat(" text-white underline ");
-  const inActive = style;
+  const style = `" px-4 duration-200 hover:underline ${theme ? " text-darkSecond " : " text-[#86198f] "}"`;
+  const active = style.concat(" underline");
+  const inActive = style.concat(" decoration-none");
   return (
     <Link className={match ? active : inActive} to={to} {...props}>
       {children}
