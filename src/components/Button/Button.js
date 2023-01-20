42750b9560e397d4add8bf348dc8fcfab250ee05
Toufiquer <toufiquerabc@gmail.com>
2023-01-21 00:06:05 +0600

    add button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { ThemeMode2 } from "../../App";
-import { FolderIcon, ShoppingCartIcon, TrashIcon } from "@heroicons/react/24/solid";
+import { FolderIcon, ShoppingCartIcon, TrashIcon, UserPlusIcon, UserMinusIcon } from "@heroicons/react/24/solid";
 const buttonStyle = "px-4 py-2 cursor-pointer text-semibold w-full rounded max-w-[400px]";
 export const AddButton = ({ id, children, handler }) => {
   const [theme2] = useContext(ThemeMode2);
@@ -52,6 +52,28 @@ export const DetailsPage = ({ navigate, children, path }) => {
   return (
     <button className={`${buttonStyle} ${view} ${hoverView}  mx-auto `} onClick={() => navigate(path)}>
       <div className="flex items-center justify-center">{children}</div>
+    </button>
+  );
+};
+export const AddUser = ({ id, children, handler }) => {
+  const [theme2] = useContext(ThemeMode2);
+  const { add, hoverAdd } = theme2;
+  return (
+    <button className={`${buttonStyle}  ${add} ${hoverAdd} text-sm`} onClick={(e) => handler(e, id)}>
+      <div className="flex items-center justify-center">
+        {children} <UserPlusIcon className="h-6 w-6 ml-2"></UserPlusIcon>
+      </div>
+    </button>
+  );
+};
+export const RemoveUser = ({ id, children, handler }) => {
+  const [theme2] = useContext(ThemeMode2);
+  const { delete0, hoverDelete } = theme2;
+  return (
+    <button className={`${buttonStyle} ${delete0} ${hoverDelete} text-sm`} onClick={(e) => handler(e, id)}>
+      <div className="flex items-center justify-center">
+        {children} <UserMinusIcon className="h-6 w-6 ml-2"></UserMinusIcon>
+      </div>
     </button>
   );
 };
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,8 +1,15 @@
 import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
 import { ThemeMode2 } from "../../App";
+import { AddUser, RemoveUser } from "../Button/Button";
 
-const User = ({ user, user: { name, email } }) => {
+const User = ({ user, user: { name, email, id } }) => {
+  const handleAddUser = (e, id) => {
+    console.log(id, " => Line No: 7");
+  };
+  const handleRemoveUser = (e, id) => {
+    console.log(id, " => Line No: 7");
+  };
   const [img, setImg] = useState("");
   useEffect(() => {
     axios.get("https://randomuser.me/api/").then((data) => setImg(data.data.results[0].picture.medium));
@@ -11,15 +18,27 @@ const User = ({ user, user: { name, email } }) => {
   const { card, border } = theme2;
   console.log(img, " => Line No: 12");
   return (
-    <div className={` ${card} p-4 ${border} flex `}>
-      <img className="h-16 w-16" src={img} alt={name} />
-      <div className="text-sm ml-2">
-        <h2 className="text-xl ">
-          <span className="font-semibold">Name: {name || "Name"} </span>
-        </h2>
-        <h2>
-          <span className="font-semibold">Email: {email || "Email"}</span>
-        </h2>
+    <div className={` ${card} p-4 `}>
+      <div className={` ${border} p-4`}>
+        <div className={`flex `}>
+          <img className="h-16 w-16" src={img} alt={name} />
+          <div className="text-sm ml-2">
+            <h2 className="text-xl ">
+              <span className="font-semibold">Name: {name || "Name"} </span>
+            </h2>
+            <h2>
+              <span className="font-semibold">Email: {email || "Email"}</span>
+            </h2>
+          </div>
+        </div>{" "}
+        <div className="flex w-full justify-evenly my-1 gap-1">
+          <AddUser id={id} handler={handleAddUser}>
+            Add
+          </AddUser>
+          <RemoveUser id={id} handler={handleRemoveUser}>
+            Remove
+          </RemoveUser>
+        </div>
       </div>
     </div>
   );
